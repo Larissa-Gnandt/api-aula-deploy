f86b1d6f113d3dcf4349477327e6032b899d72e7
Larissa-Gnandt <larissagnandt@gmail.com>
2023-09-06 17:02:06 -0300

    Refatoração do projeto

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const knex = require("./conexao");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.json("API está ok!");
+  try {
+    const racas = await knex("racas");
+    return res.json(racas);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro do servidor" });
+  }
 });
 
 // const port = process.env.PORT || 3000; dá pra se fazer assim, o .ENV deve estar próximo ao packge.json
